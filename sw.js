0d8d5db5f34c7077121242eacc247826f7fdd473
wael3ita-rgb <wael3ita@gmail.com>
2026-09-24 21:59:26 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+const CACHE = 'tetris-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
+
